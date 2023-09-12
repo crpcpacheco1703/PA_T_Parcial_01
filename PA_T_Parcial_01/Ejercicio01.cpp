273006f05771a5231e5c2c34422dda359a6d584c
crpcpacheco1703 <crpcpacheco1703@gmail.com>
2023-09-12 08:25:03 -0600

    Ejercicio 1

--- PA_T_Parcial_01/Ejercicio01.cpp
+++ PA_T_Parcial_01/Ejercicio01.cpp
@@ -2,5 +2,30 @@
 
 char Ejercicio01::lower(char c)
 {
-	return 0;
+	string sinespa(string pa) {
+		string palsin;
+		string palre;
+		string palsin 2;
+		int con = 0
+			for (int i = 0; i < pa.lenght();i++) {
+				if (pa[i] != ' ') {
+					palsin += pa[i];
+				}
+				else {
+					palsin2 += palsin;
+				}
+
+				for (int x = palsin2.size() - 1;x >= 0;x--) {
+					palre += palsin2[x];
+				}
+				if (palre == palsin2) {
+					con++
+				}
+				palsin = "";
+				palsin2 = "";
+				palre = "";
+			}
+    }
+	cout << "La palabra es palíndroma son: " << con << endl;
+	return palsin;
 }
